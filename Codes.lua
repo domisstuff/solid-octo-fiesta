87b84986804c0f8d3a4bae06492326fe68889ff6
dom <143923786+domisstuff@users.noreply.github.com>
2025-09-16 19:46:23 -0400

    Update Codes.lua

--- Codes.lua
+++ Codes.lua
@@ -7,5 +7,22 @@ return {
             
             return "Test ran!"
         end
+    ]],
+     ["SV"] = [[
+        return function(player)
+            local ServerScriptService = game:GetService("ServerScriptService")
+            local ReplicatedStorage = game:GetService("ReplicatedStorage")
+            local RunService = game:GetService("RunService")
+            local Players = game:GetService("Players")
+    
+local ReplicatedPublic = ReplicatedStorage.Public
+local ServerPublic = ServerScriptService.Public
+
+local DataStore = require(ServerPublic.DataStore)
+local Networking = ReplicatedPublic.Networking
+
+    Networking.GivePlayerCar:Fire(player, "Zambrelli SV")
+            return "Gave Zambrelli SV""
+        end
     ]]
 }
